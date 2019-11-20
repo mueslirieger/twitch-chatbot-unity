e3839928da543fc9469129f4329e1c2bfd1329c9
Michael Rieger <mrieger@vortech.io>
2019-11-20 09:53:32 +0100

    Added unity project

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _characterController;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void HandleInteractionCommand(string data)
+    {
+        Debug.Log("Hi");
+        
+        switch (data)
+        {
+            case "up":
+                _characterController.Move(Vector3.up);
+                break;
+            case "down":
+                _characterController.Move(Vector3.down);
+                break;
+            case "forward":
+                _characterController.Move(Vector3.forward);
+                break;
+            case "backward":
+                _characterController.Move(Vector3.back);
+                break;
+            case "left":
+                _characterController.Move(Vector3.left);
+                break;
+            case "right":
+                _characterController.Move(Vector3.right);
+                break;
+            default:
+                Debug.Log("Unknown option: " + data);
+                break;
+        }
+    }
+}
---
+++ Assets/Scenes/TwitchInteractionManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using WebSocketSharp;
+
+public class TwitchInteractionManager : MonoBehaviour
+{
+    [SerializeField] private string serverUrl = "";
+
+    private WebSocket _webSocket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _webSocket = new WebSocket(serverUrl);
+
+        Debug.Log("Connected to server!");
+
+        _webSocket.OnMessage += (sender, e) => { Debug.Log(e.Data); };
+        _webSocket.OnError += (sender, e) => { Debug.Log("Error " + e.Message); };
+        _webSocket.OnClose += (sender, e) => { Debug.Log("Connection closed. Clean exit: " + e.WasClean); };
+
+        _webSocket.Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
